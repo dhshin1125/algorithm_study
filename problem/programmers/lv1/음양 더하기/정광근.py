b51759123a5df8419c13267e6fbb71ecf5154989
gwang geun <jgk8866@naver.com>
2023-08-02 23:32:14 +0900

    Delete problem/programmers/lv1/음양 더하기 directory

--- problem/programmers/lv1/음양 더하기/정광근.py
+++
@@ -1,6 +0,0 @@
-def solution(absolutes, signs):
-    answer = 123456789
-    for i in range(len(absolutes)):
-        if signs[i] == False:
-            absolutes[i] = -absolutes[i] 
-    return sum(absolutes)
