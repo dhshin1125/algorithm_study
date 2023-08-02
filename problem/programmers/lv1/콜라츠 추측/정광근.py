6a8834a84943dcc1bd47d19b234f119fec3bb8ad
gwang geun <jgk8866@naver.com>
2023-08-02 23:26:16 +0900

    제출합니다

---
+++ problem/programmers/lv1/콜라츠 추측/정광근.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = -1
+    count = 0
+        
+    for i in range(500):
+        if num == 1:
+            break
+        if num % 2 == 0:
+            count += 1
+            num = num / 2
+        else:
+            count += 1
+            num = num * 3 + 1
+    if count == 500:
+        count = -1
+    
+    return count
